83bcd7873d39a5b1a37b601c8e60052b694169b0
Josh Cook <joshuacook0023@gmail.com>
2019-04-17 06:31:46 -0400

    test: no failure on empty input graph

--- tests/testthat/test-filter_component_size.R
+++ tests/testthat/test-filter_component_size.R
@@ -20,4 +20,8 @@ test_that("filter out components by number of nodes", {
 
     expect_equal(vcount(filter_component_size(gr_bind, max_size = 11)), 10)
     expect_equal(count_components(filter_component_size(gr_bind, max_size = 11)), 1)
+
+    empty_gr <- tidygraph::create_empty(0)
+    expect_identical(filter_component_size(empty_gr), empty_gr)
+    expect_equal(igraph::vcount(filter_component_size(empty_gr)), 0)
 })

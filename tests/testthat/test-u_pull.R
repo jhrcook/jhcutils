a9d63f7be9088cbd743a22740456f5dd162f8622
Josh Cook <joshuacook0023@gmail.com>
2019-04-20 10:14:52 -0400

    test: for na.rm and sorted options

--- tests/testthat/test-u_pull.R
+++ tests/testthat/test-u_pull.R
@@ -3,9 +3,26 @@ context("test-u_pull")
 test_that("pull unique values from a data table", {
     library(datasets)
     library(magrittr)
-    n_gears <- sort(unique(mtcars$gear))
+    n_gears <- unique(mtcars$gear)
     new_n_gears <- mtcars %>%
-        u_pull(gear) %>%
-        sort()
+        u_pull(gear)
+    expect_true(all(n_gears %in% new_n_gears))
+
+    n_gears <- sort(n_gears)
+    new_n_gears <- mtcars %>%
+        u_pull(gear, sorted = TRUE)
     expect_equal(n_gears, new_n_gears)
+
+    ozone <- unique(airquality$Ozone)
+    ozone <- ozone[!is.na(ozone)]
+    new_ozone <- airquality %>%
+        u_pull(Ozone, na.rm = TRUE)
+    expect_equal(ozone, new_ozone)
+
+
+    ozone <- unique(airquality$Ozone)
+    ozone <- sort(ozone[!is.na(ozone)])
+    new_ozone <- airquality %>%
+        u_pull(Ozone, sorted = TRUE, na.rm = TRUE)
+    expect_equal(ozone, new_ozone)
 })
